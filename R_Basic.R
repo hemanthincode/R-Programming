6fd2c5b6227670be7630e41247e768c9092b175c
B Hemanth Kumar <153267502+hemanthincode@users.noreply.github.com>
2024-06-05 16:46:24 +0530

    Add files via upload

---
+++ R_Basic.R
@@ -0,0 +1,108 @@
+2-1
+
+29+20
+
+88000 + 12229
+
+13000 - 12000
+
+60*3
+
+10000/10
+
+10000%%10
+
+10002%%10
+
+45^2
+
+5^2
+
+a = 6
+
+class(a)
+
+ab <- 67
+
+bool = TRUE
+class(bool)
+
+bool = 'TRUE'
+class(bool)
+
+string <- 'Data Science'
+class(string)
+
+print(string)
+
+10*30/10
+
+10+5*20
+
+exp(1)
+exp(10)
+exp(3)
+
+sqrt(100)
+sqrt(121)
+
+pi
+
+2*pi*345
+
+x <- 56
+y <- 35
+z <- 65
+
+x
+x*y/z
+
+This.year <- 2024
+This.year
+
+value<-c(1,2,3,4,5,6)
+value2<-c(22,33,44,55,66,77,88)
+
+# repetation
+rep(1,10)
+
+# sequence
+seq(1,20)
+seq(2,50,by=4)
+
+
+x<-c(1,2,3,4,5,6)
+y<-c(2,1,4,8,6,7)
+
+x+y
+
+x * 4
+
+y<-c(seq(20,2,by=-2))
+y
+
+sqrt(x)
+
+
+x <- c(2,0,0,2)
+x[1]
+
+x[-1]
+
+x[1] <- 4
+
+x
+
+y <- c(9,9,9,9)
+y
+y==9
+
+y <- c(39,9,9,59)
+y
+y==9
+y[4] = 45
+y
+
+y<9
+
+y>9
