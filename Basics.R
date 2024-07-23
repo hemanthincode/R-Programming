7f2047edb93d6b0ffd0cc29801ac39763a0603ee
B Hemanth Kumar <153267502+hemanthincode@users.noreply.github.com>
2024-07-23 10:23:46 +0530

    Add files via upload

---
+++ Basics.R
@@ -0,0 +1,78 @@
+x= 10
+x
+class(x)
+
+x2 = 12.5
+x2
+class(x2)
+x3 = as.integer(calls)
+x3
+class(x3)
+x4 = as.numeric(x4)
+x4
+class(x4)
+x4
+
+x5= 'apple'
+x5
+class(x5)
+
+x6 = as.character(x2)
+x6
+class(x6)
+
+
+x7= TRUE
+x7
+class(x7)
+as.character(x7)
+#---------------------------------------------
+
+x8= c('m','f','m','f','m','f','f','f')
+x8
+class(x8)
+
+x9= as.factor(x8)
+x9
+class(x9)
+nlevels(x9)
+summary(x9)
+length(x9)
+#-----------------------------------------------------
+
+age= c(22,34,33,55,45,45,45,64,65)
+age
+length(age)
+class(age)
+summary(age)
+
+id= 1:10
+id
+age
+age[1]
+mean(age)
+median(age)
+sd(age)
+var(age)
+quantile(age)
+quantile(age,0.25)
+#----------------------------------------------------------
+
+
+
+
+
+ 
+
+
+
+
+
+ 
+
+
+
+
+
+
+
